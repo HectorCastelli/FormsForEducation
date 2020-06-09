170dfee78fb88998338bf889a54b6594688de2d6
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2020-06-09 20:16:01 +0200

    Fixed MimeType transpilation issue

--- src/drive/folderOpener.ts
+++ src/drive/folderOpener.ts
@@ -3,7 +3,5 @@ function getFormsInFolder(
 ): GoogleAppsScript.Drive.FileIterator {
   const folderId = folderUrl.toString().split("/").pop();
   const driveFolder = DriveApp.getFolderById(folderId);
-  return driveFolder.getFilesByType(
-    GoogleAppsScript.Base.MimeType.GOOGLE_FORMS.toString()
-  );
+  return driveFolder.getFilesByType(MimeType.GOOGLE_FORMS);
 }
